da0a53012ba22425339d1100794f510d9851e6d8
MykolaIvanov1 <l0_t0s@ukr.net>
2023-04-18 12:47:04 +0300

    add hw-08

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,38 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+// Створює розмітку галереї
+
+function createGalleryMarkup(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class="gallery__item">
+          <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+        </li>`
+    )
+    .join('');
+}
+
+const addElementMarkup = createGalleryMarkup(galleryItems);
+
+// Додає розмітку галереї в HTML документ
+
+galleryEl.innerHTML = addElementMarkup;
+
+new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,16 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframeEl = document.querySelector('iframe');
+const player = new Player(iframeEl);
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+const onPlay = function (data) {
+  const roundetTime = Math.round(data.seconds);
+  localStorage.setItem(LOCAL_STORAGE_KEY, roundetTime);
+};
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+const currentTime = localStorage.getItem(LOCAL_STORAGE_KEY);
+player.setCurrentTime(currentTime);
